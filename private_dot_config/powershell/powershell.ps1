4099e74b3cd3c8a03cb5571548d2dbe049f6ff4b
lorilorimori <>
2026-06-19 15:14:39 +0400

    ps shell completions

--- private_dot_config/powershell/powershell.ps1
+++ private_dot_config/powershell/powershell.ps1
@@ -2,11 +2,6 @@
 
 $env:PATH_PS_CONFIG_DIR = "$HOME/.config/powershell"
 
-# misc configuration
-. "$env:PATH_PS_CONFIG_DIR/settings.ps1"
-
-# aliases
+. "$env:PATH_PS_CONFIG_DIR/shell.ps1"
 . "$env:PATH_PS_CONFIG_DIR/aliases/aliases.ps1"
-
-# tools
 . "$env:PATH_PS_CONFIG_DIR/tools/tools.ps1"
--- private_dot_config/powershell/shell.ps1
+++ private_dot_config/powershell/shell.ps1
@@ -5,5 +5,9 @@ $OutputEncoding = [System.Text.Encoding]::UTF8
 # tab completion from history
 # PSReadLine may not be avaibale as a directive in non-interracatble sessiobns
 try {
-    Set-PSReadlineOption -PredictionSource History
-} catch {}
+    Set-PSReadlineOption -PredictionSource HistoryAndPlugin
+    Set-PSReadLineOption -PredictionViewStyle ListView
+    Set-PSReadLineOption -EditMode Windows
+} catch {
+    Write-Warning "PSReadLine configuration failed: $_"
+}
