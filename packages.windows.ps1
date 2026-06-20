bd387f26a3f1f895f79aa5261401787530773bb6
lorilorimori <>
2026-06-21 02:15:42 +0400

    powertoys

--- packages.windows.ps1
+++ packages.windows.ps1
@@ -1,9 +1,12 @@
 winget install chezmoi
 winget install Task.task
+
+winget install Microsoft.PowerShell
+winget install Microsoft.PowerToys
+
 winget install Volta.Volta
 winget install astral-sh.uv
 winget install Git.Git
-winget install Microsoft.PowerShell
 
 Invoke-WebRequest https://aka.ms/vs/17/release.ltsc.17.8/vs_buildtools.exe -OutFile ~\AppData\Local\Temp\vs_buildtools.exe
 Start-Process -Wait -PassThru -FilePath ~\AppData\Local\Temp\vs_buildtools.exe -ArgumentList '--passive --wait --norestart --nocache --add Microsoft.VisualStudio.Component.Roslyn.Compiler --add Microsoft.Component.MSBuild --add Microsoft.VisualStudio.Component.CoreBuildTools --add Microsoft.VisualStudio.Workload.MSBuildTools --add Microsoft.VisualStudio.Component.VC.CoreBuildTools --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.VC.Redist.14.Latest --add Microsoft.VisualStudio.Component.VC.CMake.Project --add Microsoft.VisualStudio.Component.VC.ATL --add Microsoft.VisualStudio.Component.VC.ASAN  --add Microsoft.VisualStudio.Component.TextTemplating --add Microsoft.VisualStudio.Component.VC.CoreIde --add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Core --add Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset --add Microsoft.VisualStudio.Component.VC.Llvm.Clang --add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Llvm.Clang --add Microsoft.VisualStudio.Component.Windows10SDK.20348 --add Microsoft.VisualStudio.Workload.VCTools'
