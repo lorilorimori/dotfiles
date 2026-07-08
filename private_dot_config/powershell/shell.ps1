8a751dbbeb63d11fc8399d75614dd7dcef277cc1
lorilorimori <>
2026-07-08 22:34:09 +0400

    ps zsh-styled completion

--- private_dot_config/powershell/shell.ps1
+++ private_dot_config/powershell/shell.ps1
@@ -2,12 +2,14 @@
 # UTF-8 output encoding for compatibility with PowerShell < 7
 $OutputEncoding = [System.Text.Encoding]::UTF8
 
-# tab completion from history
-# PSReadLine may not be avaibale as a directive in non-interracatble sessiobns
 try {
+    # tab completion from history
+    # PSReadLine may not be avaibale as a directive in non-interracatble sessiobns
     Set-PSReadlineOption -PredictionSource HistoryAndPlugin
     Set-PSReadLineOption -PredictionViewStyle ListView
     Set-PSReadLineOption -EditMode Windows
+    # zsh-like еab completion
+    Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 } catch {
     Write-Warning "PSReadLine configuration failed: $_"
 }
